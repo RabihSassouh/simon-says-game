d4987e8fdc486bbdc59d934caf02b9810bb3b66d
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-03-02 12:18:13 +0200

    added generateRandom function that generates a random color from the array "tiles"

--- js/main.js
+++ js/main.js
@@ -6,10 +6,15 @@ let x=0;
 function playGame(){
     playBtn.textContent="Wait for computer sequence!";    
 }
-
+function generateRandom(){
+    const tiles=["green","red","blue","yellow"];
+    const random=tiles[Math.floor(Math.random()*tiles.length)];
+    return random;
+}
 function nextLevel(){
     x++;
     currentLevel.textContent =x;
     const newSequence=[...sequence];
+    newSequence.push(generateRandom());
 }
 playBtn.addEventListener("click",playGame);
