b86f7b324c82da1a17e0875b9b2323aedc3ae302
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-03-02 11:45:40 +0200

    added nextLevel function that increments level score and copies the previous sequence into  a new one

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
+let sequence=[]
+let playerSequence=[]
 const playBtn= document.getElementById("play");
 let currentLevel= document.getElementById("level");
-
+let x=0;
 function playGame(){
-    playBtn.textContent="Wait for computer sequence!";
-    console.log("test");
+    playBtn.textContent="Wait for computer sequence!";    
 }
 
+function nextLevel(){
+    x++;
+    currentLevel.textContent =x;
+    const newSequence=[...sequence];
+}
 playBtn.addEventListener("click",playGame);
