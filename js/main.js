8dfaaa315934ba64d821dbc5a162eb8242545314
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-03-02 20:22:12 +0200

    clean main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ const playBtn = document.getElementById("play");
 const info = document.getElementById("info");
 const board = document.querySelector(".board");
 let currentLevel = document.getElementById("level");
-let highScore= document.getElementById("high-score");
+let highScore = document.getElementById("high-score");
 let x = 0;
-let highScoreNum=0;
+let highScoreNum = 0;
 function handleClick(tile) {
   const index = playerSequence.push(tile) - 1;
   const remainingTaps = sequence.length - playerSequence.length;
@@ -60,9 +60,9 @@ function generateRandom() {
 function nextLevel() {
   x++;
   currentLevel.textContent = x;
-  if (x > highScoreNum){
-    highScoreNum=x;
-    highScore.textContent=highScoreNum;
+  if (x > highScoreNum) {
+    highScoreNum = x;
+    highScore.textContent = highScoreNum;
   }
   board.classList.add("unclickable");
   info.textContent = "Memorize the computer sequence";
