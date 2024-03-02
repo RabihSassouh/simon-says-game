4f7eb3c2a44f430b3503e5e792a8c7b2d4ce6918
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-03-02 14:54:20 +0200

    added functions clickTile and playLevel

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
-let sequence=[]
-let playerSequence=[]
-const playBtn= document.getElementById("play");
-let currentLevel= document.getElementById("level");
-let x=0;
-function playGame(){
-    playBtn.textContent="Wait for computer sequence!";    
+let sequence = [];
+let playerSequence = [];
+const playBtn = document.getElementById("play");
+let currentLevel = document.getElementById("level");
+let x = 0;
+function playGame() {
+  playBtn.textContent = "Wait for computer sequence!";
+  nextLevel();
 }
-function generateRandom(){
-    const tiles=["green","red","blue","yellow"];
-    const random=tiles[Math.floor(Math.random()*tiles.length)];
-    return random;
+
+function clickTile(color) {
+  const tileColor = document.querySelector(`[data-tile='${color}']`);  
+ 
+  tileColor.classList.remove("inactive");
+  setTimeout(() => {
+    tileColor.classList.add("inactive");
+  }, 400);
 }
-function nextLevel(){
-    x++;
-    currentLevel.textContent =x;
-    const newSequence=[...sequence];
-    newSequence.push(generateRandom());
+
+function playLevel(newSequence) {
+  newSequence.forEach((color) => {
+    setTimeout(() => {
+      clickTile(color);
+    }, 600);
+  });
 }
-playBtn.addEventListener("click",playGame);
+function generateRandom() {
+  const tiles = ["green", "red", "blue", "yellow"];
+  const random = tiles[Math.floor(Math.random() * tiles.length)];
+  return random;
+}
+
+function nextLevel() {
+  x++;
+  currentLevel.textContent = x;
+  const newSequence = [...sequence];
+  newSequence.push(generateRandom());
+  playLevel(newSequence);
+}
+
+playBtn.addEventListener("click", playGame);
